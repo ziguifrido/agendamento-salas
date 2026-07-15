b929865b3c8a93067b67c455518ce7921cfdf9b9
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2026-07-15 01:48:16 -0400

    fix: atualiza o cache do aplicativo

--- web/templates/static/sw.js
+++ web/templates/static/sw.js
@@ -1,15 +1,19 @@
-const CACHE = 'salas-v1';
+const CACHE = 'salas-v19';
 const ASSETS = ['/', '/static/css/tokens.css', '/static/css/app.css', '/static/js/htmx.js', '/static/manifest.webmanifest', '/static/icons/icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(caches.open(CACHE).then(cache => cache.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    cache.put(event.request, copy);
     return response;
-  })));
+  }))));
 });
